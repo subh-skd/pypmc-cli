e9ebcc2c785a0a3238ef1f2c2ee755af7c276b4c
subh-skd <internship.subhendu@gmail.com>
2026-03-27 11:30:06 +0530

    added example project

---
+++ pypmc-example-project/main.py
@@ -0,0 +1,12 @@
+"""
+pypmc-example-project - this is an example project using pypmc
+"""
+
+
+def main():
+    print("Hello from pypmc-example-project!")
+    print("Edit this file to get started.")
+
+
+if __name__ == "__main__":
+    main()
